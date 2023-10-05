7028e9b9dbc5135b5805aaa3d2df990740711009
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-10-05 21:51:22 +0530

    Added User Table

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('651d7b1ed34325a020feae27')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('651edb9611142c962e18ff2f')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +35,18 @@ app.use(errorController.get404);
 mongoose
   .connect('mongodb+srv://saurabhpawar71100:Saurabh%40123@cluster0.vkpbsmc.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'John',
+          email: 'john@mail.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save();
+      }
+    })
     app.listen(3000);
   })
   .catch(err => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const userId = req.user._id;
   const product = new Product({
     title: title,
     price: price,
     description: description,
-    imageUrl: imageUrl
+    imageUrl: imageUrl,
+    userId: userId
   });
   product
     .save()
